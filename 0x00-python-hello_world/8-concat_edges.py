66a8ea750cb4656b56e92e9db8d19a9cde784f79
Gareth Brickman <977@holbertonschool.com>
2019-09-09 22:16:28 +0000

    Edit 8. for line character count

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "{0} {1} {2} {3}".format("object-oriented", "programming", "with", "Python")
+str = "{0} {1} {2} {3}".format("object-oriented","programming","with","Python")
 print(str)
