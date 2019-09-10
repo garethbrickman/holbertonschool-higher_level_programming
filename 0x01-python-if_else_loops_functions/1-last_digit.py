c43cb2cd3242f217113554ab8055f251e184865c
Gareth Brickman <977@holbertonschool.com>
2019-09-10 21:03:26 +0000

    Edit 1. file for 0 condition

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 if number == 0:
-    print('Last digit of 0 is 0 and is 0')
+    print('Last digit of {} is {} and is 0'.format(number, number % 10))
 
 elif number < 0:
 
