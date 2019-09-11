f6866e5cd82ec6e71e48fe202b95d50e0ba837a9
Gareth Brickman <977@holbertonschool.com>
2019-09-11 00:46:18 +0000

    Edit 8. for empty str edge case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     strup = ''
+    if len(str) < 1:
+        print(("{}").format(str))
     for i in range(len(str)):
         if(str[i] >= 'a' and str[i] <= 'z'):
             strup = strup + chr((ord(str[i]) - 32))
