30b69c354a92cb37d1c55923b23526cfe7fc3785
Gareth Brickman <977@holbertonschool.com>
2019-09-11 02:00:36 +0000

    Edit 1. for making modlus of negative a negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-absnum = abs(number)
-modnum = absnum % 10
+modnu = abs(number) % 10
+
+if number < 0:
+    modnu *= -1
 
 if number == 0:
-    print('Last digit of {} is {} and is 0'.format(number, modnum))
-elif modnum == 0:
-    print('Last digit of {} is {} and is 0'.format(number, modnum))
-elif modnum > 5:
-    print('Last digit of {} is {} and is greater than 5'
-          .format(number, modnum))
-elif modnum < 6 and modnum != 0:
-    print('Last digit of {} is {} and is less than 6 and not 0'
-          .format(number, modnum))
+    print("Last digit of", number, "is", modnu, "and is 0")
+if modnu == 0:
+    print("Last digit of", number, "is", modnu, "and is 0")
+elif modnu > 5:
+    print("Last digit of", number, "is", modnu, "and is greater than 5")
+else:
+    print("Last digit of", number, "is", modnu, "and is less than 6 and not 0")
 
 # if number == 0:
 #     print('Last digit of {} is {} and is 0'.format(number, number % 10))
