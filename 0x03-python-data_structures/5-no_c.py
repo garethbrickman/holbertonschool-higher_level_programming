17559a9d63876db4da3e9e1f513822d8047e730f
Gareth Brickman <977@holbertonschool.com>
2019-09-18 01:17:45 +0000

    Add 5. file

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    strup = ''
+    if len(my_string) < 1:
+        return
+    else:
+        for i in range(len(my_string)):
+            if(my_string[i] != 'c' or my_string[i] != 'C'):
+                strup = strup + my_string[i]
+                return strup
