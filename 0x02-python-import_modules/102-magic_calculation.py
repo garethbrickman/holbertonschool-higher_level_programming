1489fba22b1517fd99c8a9cb842cf0aa0aa33749
Gareth Brickman <977@holbertonschool.com>
2019-09-18 19:34:30 +0000

    Add 102.

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+
+    return sub(a, b) if 1 else None
+
+
+
+import dis
+dis.dis(magic_calculation)
