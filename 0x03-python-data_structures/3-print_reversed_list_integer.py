b67f0f51e6d2a23062f7bca15f1c0f9dc92235e5
Gareth Brickman <977@holbertonschool.com>
2019-09-18 00:46:24 +0000

    Edit 3. for empty list check

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-        for x in reversed(my_list):
-                print("{:d}".format(x))
+        if len(my_list) < 1:
+                return
+        else:
+                for x in reversed(my_list):
+                        print("{:d}".format(x))
