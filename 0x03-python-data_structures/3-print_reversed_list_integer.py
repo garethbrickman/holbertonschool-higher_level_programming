db15ca677528859125f7960e1c0a3bbe979e6c61
Gareth Brickman <977@holbertonschool.com>
2019-09-18 03:08:34 +0000

    Edit 3. to include None case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-        if len(my_list) < 1:
+        if my_list is None:
                 return
         else:
                 for x in reversed(my_list):
