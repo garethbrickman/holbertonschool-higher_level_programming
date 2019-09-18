f45c8a488cd58c2a0d4e723fb5e8417f4e8780c1
Gareth Brickman <977@holbertonschool.com>
2019-09-18 02:50:50 +0000

    Edit 8. for empty string case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
+    if len(sentence) < 1:
+        first = None
+    else:
+        first = sentence[0]
     tuple = length, first
     return tuple
