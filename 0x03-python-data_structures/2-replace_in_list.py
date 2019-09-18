0c9809d5bf8711a79ed51f4bdcc299f8389136e4
Gareth Brickman <977@holbertonschool.com>
2019-09-18 00:16:42 +0000

    Add 2.

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+        if idx < 0:
+                return my_list
+        elif idx >= len(my_list):
+                return my_list
+        else:
+                my_list.insert(idx, element)
+                my_list.remove(idx + 1)
+                return my_list
