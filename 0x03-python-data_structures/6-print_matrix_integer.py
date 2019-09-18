d0ab9ddecc2f00287c7e53d73216c0390b5b61e9
Gareth Brickman <977@holbertonschool.com>
2019-09-18 02:13:39 +0000

    Add 6.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print("{:d} ".format(matrix[i][j]), end="")
