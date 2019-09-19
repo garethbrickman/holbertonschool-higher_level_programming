96ad5639c4d4642f953e1f042e6f209868f94975
Gareth Brickman <977@holbertonschool.com>
2019-09-19 20:02:57 +0000

    Add 0.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared2 = []
+    for x in range(len(matrix)):
+        squared = []
+        for y in range(len(matrix[x])):
+            squared.append(matrix[x][y] ** 2)
+        squared2.append(squared)
+    return squared2
