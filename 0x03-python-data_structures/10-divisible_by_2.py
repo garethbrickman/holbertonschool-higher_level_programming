0ef05a8feeda63d4dcc466700861bfda7995f8e8
Gareth Brickman <977@holbertonschool.com>
2019-09-19 01:19:44 +0000

    Edit 10. for empty list case

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
     new_list1 = my_list.copy()
     for i in range(len(new_list1)):
         if new_list1[i] == 0:
