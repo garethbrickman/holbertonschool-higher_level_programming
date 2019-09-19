c43b15fad5bceebb1412a4d51d7effdf48410f31
Gareth Brickman <977@holbertonschool.com>
2019-09-19 01:22:53 +0000

    Edit 10. to swap if elif order

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,8 +6,8 @@ def divisible_by_2(my_list=[]):
     for i in range(len(new_list1)):
         if new_list1[i] == 0:
             new_list1[i] = False
-        elif (abs(new_list1[i]) % 2) != 0:
-            new_list1[i] = False
-        else:
+        elif (abs(new_list1[i]) % 2) == 0:
             new_list1[i] = True
+        else:
+            new_list1[i] = False
     return(new_list1)
