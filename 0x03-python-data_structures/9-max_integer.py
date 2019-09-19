14197f7c70e17d76a5180daa1e9657c3ef57a392
Gareth Brickman <977@holbertonschool.com>
2019-09-19 00:43:03 +0000

    Edit 9. for new method

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,10 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
+    if len(my_list) < 1:
+        return None
+    big = my_list[0]
     for i in my_list:
-        if my_list[i] > my_list[i + 1]:
-            return my_list[i]
-        else:
-            return my_list[i]
+        if i > big:
+            big = i
+    return big
