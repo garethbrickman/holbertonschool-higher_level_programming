fed12fbe4434a01061c33ace234b1fa30625fab9
Gareth Brickman <977@holbertonschool.com>
2019-09-19 01:29:08 +0000

    Edit 10. for 0 experiment

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,9 +4,9 @@ def divisible_by_2(my_list=[]):
         return
     new_list1 = my_list.copy()
     for i in range(len(new_list1)):
-        if new_list1[i] == 0:
-            new_list1[i] = True
-        elif (abs(new_list1[i]) % 2) == 0:
+#        if new_list1[i] == 0:
+#            new_list1[i] = True
+        if (abs(new_list1[i]) % 2) == 0:
             new_list1[i] = True
         else:
             new_list1[i] = False
