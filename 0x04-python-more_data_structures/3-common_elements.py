cf33f56ebc5d1c5a6fabdc3dbfde8603c0646d75
Gareth Brickman <977@holbertonschool.com>
2019-09-20 02:02:37 +0000

    Edit 3. for s1 & s1 comparison

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(s1, s2):
-    for x in s1:
-        for y in s2:
-            if x == y:
-                return x
+    if (s1 & s2):
+        return (s1 & s2)
