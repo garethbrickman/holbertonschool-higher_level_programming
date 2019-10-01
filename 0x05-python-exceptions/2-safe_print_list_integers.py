dd2a11a3a0d7ec456690d70e74c5d8c7178c502b
Gareth Brickman <977@holbertonschool.com>
2019-10-01 00:09:43 +0000

    Edit 2. for pep8

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-      y = 0
-      try:
-            for i in range(x):
-                  print("{:d}".format(my_list[i]), end="")
-                  y += 1
-      except ValueError:
-            pass
-      print()
-      return y
+    y = 0
+    try:
+        for i in range(x):
+                print("{:d}".format(my_list[i]), end="")
+                y += 1
+    except ValueError:
+        pass
+    print()
+    return y
