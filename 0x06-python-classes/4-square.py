0e97ddbdd0e03190632e181199e4443539c54391
Gareth Brickman <977@holbertonschool.com>
2019-10-02 00:39:48 +0000

    Add 4.

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Optional instantiation for size, must be int and >= 0"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter"""
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns current square area"""
+        return (self.__size * self.__size)
