f96ce402f2f1b72dcf195ec82ec0c4226b23b3bd
Gareth Brickman <977@holbertonschool.com>
2019-10-03 18:44:35 +0000

    Add 0., README, tests dir

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+     Module for addition functions
+"""
+
+
+def add_integer(a, b=98):
+    """ Addition function """
+    pass
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This is a README for my 0x07-python-test_driven_development project!
