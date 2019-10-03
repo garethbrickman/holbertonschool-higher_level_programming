286a0461717c46c66cf64dcbdffe9ccc93c388f6
Gareth Brickman <977@holbertonschool.com>
2019-10-03 21:56:15 +0000

    Add 0. files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """
      Module for addition functions
+
+
 """
 
 
 def add_integer(a, b=98):
-    """ Addition function """
-    pass
+    """ Addition function
+
+    """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return a + b
