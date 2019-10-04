ce7ef12f5a334fde9461d46d71eacf7f45a42488
Gareth Brickman <977@holbertonschool.com>
2019-10-04 18:47:52 +0000

    Add 2. files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+     Module for matrix_divided functions
+
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """ Divide matrix function
+
+    """
+    count = len(matrix)
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise TypeError("division by zero")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    while count > -1:
+        if len(matrix[0]) != len(matrix[count -1]):
+            raise TypeError("Each row of the matrix must have the same size")
+        count -= 1
+
+    newmatrix2 = []
+    for x in range(len(matrix)):
+        newmatrix = []
+        for y in range(len(matrix[x])):
+            newmatrix.append(round(matrix[x][y] / div, 2))
+        newmatrix2.append(newmatrix)
+    return newmatrix2
