9bb76fec1dfe0dea4433ba86db98009964067328
Gareth Brickman <977@holbertonschool.com>
2019-10-04 21:53:04 +0000

    Add 5. files

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+     Module for text_indentation functions
+
+
+"""
+
+
+def text_indentation(text):
+    """ Text indentation function
+
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    newtext = ""
+    delims = ".?:"
+
+    for i in text:
+        newtext += text[i]
+        if text[i] in delims:
+            newtext += "\n\n"
+    print(newtext, end="")
+
+
+    # indent1 = text.split(':')
+    # indent2 = ':\n\n'.join(indent1)
+    # indent3 = indent2.split('?')
+    # indent4 = '?\n\n'.join(indent3)
+    # indent5 = indent4.split('.')
+    # indent6 = '.\n\n'.join(indent5)
+    # print(indent6)
