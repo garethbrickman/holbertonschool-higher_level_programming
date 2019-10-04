d01f2c9e603aae226d3785287c422dcfe4c3c991
Gareth Brickman <977@holbertonschool.com>
2019-10-04 21:00:27 +0000

    Edit 4. to cast flaot to int

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,7 +12,9 @@ def print_square(size):
     """
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    if not isinstance(size, int):
+    if isinstance(size, float) and size >= 0:
+        size = int(size)
+    if not isinstance(size, int) and not isinstance(size, float):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
