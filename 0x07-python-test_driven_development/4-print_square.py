9bf7fd580a0a720ed2e6fd1aea78d51f5f16a70d
Gareth Brickman <977@holbertonschool.com>
2019-10-04 20:16:49 +0000

    Add 4. files

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+     Module for print_square functions
+
+
+"""
+
+
+def print_square(size):
+    """ Say name function
+
+    """
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        pass
+    else:
+        for x in range(size):
+            for y in range(size):
+                print("#", end="")
+            print()
