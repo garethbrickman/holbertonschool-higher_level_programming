0521b0ab6823cd8cc96522fb70b2285fe2ceb6cc
Gareth Brickman <977@holbertonschool.com>
2019-10-04 19:58:32 +0000

    Add 3. files

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+     Module for say_my_name functions
+
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ Say name function
+
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if first_name is "" and last_name is "":
+        raise TypeError("You must enter a string for either first_name or last_name")
+    if first_name is " " and last_name is " ":
+        raise TypeError("You must enter a string for either first_name or last_name")
+
+    print("My name is {} {}".format(first_name, last_name))
