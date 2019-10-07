58198efe0cbf6cb0c06d9d4b82b867b9b9d0236e
Gareth Brickman <977@holbertonschool.com>
2019-10-07 17:44:19 +0000

    Edit 2. to shorten error message

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,13 +11,14 @@ def matrix_divided(matrix, div):
 
     """
     count = len(matrix)
+    errmess = "matrix must be a matrix (list of lists) of integers/floats"
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise TypeError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(errmess)
     while count > -1:
         if len(matrix[0]) != len(matrix[count-1]):
             raise TypeError("Each row of the matrix must have the same size")
