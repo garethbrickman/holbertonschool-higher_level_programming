1c02670f230c3d5abdf7ddbf74a73a86a8773ab5
Gareth Brickman <977@holbertonschool.com>
2019-10-09 17:00:47 +0000

    Edit 101.py for pep8

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+     Module for lazy matrix multplication
+
+
+"""
 import numpy as np
+
+
 def lazy_matrix_mul(m_a, m_b):
+    """ Lazy matrix """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
