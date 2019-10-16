7013a023ae441ee3d13ea620308945bce1d38fc1
Gareth Brickman <977@holbertonschool.com>
2019-10-16 01:01:39 +0000

    Add 6.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+     Class for BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Defines base class"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
