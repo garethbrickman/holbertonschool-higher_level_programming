7a88914d4ed918f2be563470ba53ee9032541832
Gareth Brickman <977@holbertonschool.com>
2019-10-17 01:29:05 +0000

    Edit 4. to include with

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
-    f = open(filename, mode='a', encoding='utf-8')
-    f.write(text)
-    x = f.seek(0, 2)
-    f.close()
+    with open(filename, mode='a', encoding='utf-8') as f:
+        f.write(text)
+        x = f.seek(0, 2)
     return x
