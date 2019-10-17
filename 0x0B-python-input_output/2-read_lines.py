4f23e400a34be812e9a20d9ca3a8943c3cc7dd72
Gareth Brickman <977@holbertonschool.com>
2019-10-17 00:07:44 +0000

    Edit 2. for enumerate()

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     x = 0
-    y = 0
-    with open(filename) as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         for line in f:
             x += 1
     if nb_lines <= 0 or nb_lines >= x:
-        with open(filename) as f:
+        with open(filename, mode='r', encoding='utf-8') as f:
             for line in f:
                 print(line, end="")
     else:
-        with open(filename) as f:
-            for line in f:
-                while y < nb_lines:
-                    print(line, end="")
-                    y += 1
+        with open(filename, mode='r', encoding='utf-8') as f:
+            for line_no, line in enumerate(f):
+                if line_no == nb_lines:
+                    break
+                print(line, end="")
