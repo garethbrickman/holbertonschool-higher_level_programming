8e39a964167c0416f5c7bd8e320779922439525e
Gareth Brickman <977@holbertonschool.com>
2019-10-17 15:12:40 +0000

    Edit 4. for type check

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-    if issubclass(a_class, type(obj)):
-        pass
-    else:
-        return True
+    if type(obj) != a_class:
+        return isinstance(obj, a_class)
