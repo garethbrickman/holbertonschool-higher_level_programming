f35dff122006b7207941b58065bd4b1787dc60c4
Gareth Brickman <977@holbertonschool.com>
2019-10-17 18:11:33 +0000

    Edit 8. remove subclass comments

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,9 +17,6 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-"""
-     Class for Rectangle
-"""
 
 
 class Rectangle(BaseGeometry):
