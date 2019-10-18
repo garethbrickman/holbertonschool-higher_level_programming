8d548fb6c9c1d75ec01c3c8889a954f82cd55a26
Gareth Brickman <977@holbertonschool.com>
2019-10-18 01:44:05 +0000

    Add 11.

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Student class module
+"""
+
+
+class Student:
+    """ Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Makes dict representation
+        """
+        x = vars(self)
+        return x
