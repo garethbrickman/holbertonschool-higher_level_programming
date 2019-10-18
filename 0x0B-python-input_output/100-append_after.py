11738ef7a434ee2ddead94c7c12d17fb9cbcd2e7
Gareth Brickman <977@holbertonschool.com>
2019-10-18 23:11:17 +0000

    Edit 100. for list append method

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def append_after(filename="", search_string="", new_string=""):
-    f = open(filename, mode='r', encoding='utf-8')
-    for line in f:
-        if line == search_string:
-            f.close()
-            with open(filename, mode='a', encoding='utf-8') as f:
-                f.write(new_string)
+    list2 = []
+    with open(filename, mode='r', encoding='utf-8') as f:
+        x = f.readlines()
+        for line in x:
+            list2.append(line)
+            if search_string in line:
+                list2.append(new_string)
+    with open(filename, mode='w', encoding='utf-8') as f:
+        for line in list2:
+            f.write(line)
