27f092ef6057551caed09a8e33062bb24614104b
Gareth Brickman <977@holbertonschool.com>
2019-11-27 19:48:23 +0000

    [FIX] Remove field aliases

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
 -- Lists all shows containing at least one of the same genres
 -- Displays NULL if none overlap
-SELECT ts.title, tg.genre_id
+SELECT ts.title, tv_show_genres.genre_id
 FROM tv_shows as ts
-LEFT JOIN tv_show_genres as tg
-ON ts.id = tg.show_id
-ORDER BY ts.title ASC, tg.genre_id ASC
+LEFT JOIN tv_show_genres
+ON ts.id = tv_show_genres.show_id
+ORDER BY ts.title ASC, tv_show_genres.genre_id ASC
 ;
