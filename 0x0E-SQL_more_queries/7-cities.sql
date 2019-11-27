d56e00426f7f92a412a858a536faad772113aa95
Gareth Brickman <977@holbertonschool.com>
2019-11-27 00:34:01 +0000

    [ADD] 7.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Creates database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+id INT SERIAL DEFAULT VALUE PRIMARY KEY,
+state_id INT NOT NULL,
+FOREIGN KEY (state_id) REFERENCES `hbtn_0d_usa`.`states`(id),
+name VARCHAR(256) NOT NULL
+);
