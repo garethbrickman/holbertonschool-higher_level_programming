3937618dc22c9e8f81dd851bf6efe9a4167e31af
Gareth Brickman <977@holbertonschool.com>
2019-11-27 23:06:03 +0000

    [FIX] Try GROUP BY with tv_genres.name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Lists all genres with number of shows in each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+RIGHT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY 1
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC
 ;
