f7d54a1213d456e716feccc034b13831fcc9c177
Gareth Brickman <977@holbertonschool.com>
2019-11-27 19:53:53 +0000

    [FIX] keywork capitalization

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all shows without at least one of the same genres
 SELECT ts.title, tg.genre_id
 FROM tv_shows AS ts
-LEFT JOIN tv_show_genres as tg
+LEFT JOIN tv_show_genres AS tg
 ON ts.id = tg.show_id
 WHERE tg.show_id IS NULL
 ORDER BY ts.title ASC, tg.genre_id ASC
