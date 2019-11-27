f29c8a97337dec0003d78a0769999cbfe1b5b3a8
Gareth Brickman <977@holbertonschool.com>
2019-11-27 19:44:45 +0000

    [FIX] Removed field aliases

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- Lists all shows containing at least one of the same genres
-SELECT ts.title, tg.genre_id
+SELECT ts.title, tv_show_genres.genre_id
 FROM tv_shows as ts
-INNER JOIN tv_show_genres as tg
-ON ts.id = tg.show_id
-ORDER BY ts.title ASC, tg.genre_id ASC
+INNER JOIN tv_show_genres
+ON ts.id = tv_show_genres.show_id
+ORDER BY ts.title ASC, tv_show_genres.genre_id ASC
 ;
