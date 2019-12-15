6920b7fb7679ce13c37a1cc2992ef660f14b8b75
Gareth Brickman <977@holbertonschool.com>
2019-12-15 01:26:59 +0000

    [FIX] Row[0]

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1][:1] == "N":
+        if row[1][0] == "N":
             print(row)
     cur.close()
     db.close()
