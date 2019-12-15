c89f1f71b5160dfdc354e62b97feb2a44e031dd1
Gareth Brickman <977@holbertonschool.com>
2019-12-15 01:59:45 +0000

    [FIX] Add pre-print if check

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
                 .format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][:] == argv[4]:
+            print(row)
     cur.close()
     db.close()
