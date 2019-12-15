a9af1243dc417ad5e398129a0828e10334c19d8e
Gareth Brickman <977@holbertonschool.com>
2019-12-15 01:07:00 +0000

    [FIX] Try import statements before if __name__

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Lists all states from databse hbtn_0e_0_usa starting with N """
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     # argv[1] = username, [2] = password, [3] = database
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
                          host="localhost", port=3306)
