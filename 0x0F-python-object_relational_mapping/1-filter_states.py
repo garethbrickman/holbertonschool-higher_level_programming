62a38f723726cf8a093338cf268d4bf7a8ecf9ec
Gareth Brickman <977@holbertonschool.com>
2019-12-15 00:59:18 +0000

    [FIX] Use .format

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,12 +5,13 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
     # argv[1] = username, [2] = password, [3] = database
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         host="localhost", port=3306)
     # host="localhost"(default), port=3306(default)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        print('{}'.format(row))
     cur.close()
     db.close()
