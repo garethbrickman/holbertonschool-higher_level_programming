f1c326919cd3eb15e9e2548ce5ad1feb25bacdb7
Gareth Brickman <977@holbertonschool.com>
2019-12-15 01:22:51 +0000

    [FIX] Filter query in if statement, print with brackets and comma

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,9 +9,23 @@ if __name__ == "__main__":
                          host="localhost", port=3306)
     # host="localhost"(default), port=3306(default)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print('{}'.format(row))
+        if row[1][:1] == "N":
+            print("({}, \'{}\')".format(row[0], row[1]))
     cur.close()
     db.close()
+
+# if __name__ == "__main__":
+#     # argv[1] = username, [2] = password, [3] = database
+#     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+#                          host="localhost", port=3306)
+#     # host="localhost"(default), port=3306(default)
+#     cur = db.cursor()
+#     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+#     query_rows = cur.fetchall()
+#     for row in query_rows:
+#         print('{}'.format(row))
+#     cur.close()
+#     db.close()
