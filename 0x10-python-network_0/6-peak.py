af98950b45dd5c017bbbd2859742ad150ecde6d7
Gareth Brickman <977@holbertonschool.com>
2020-01-06 18:54:18 +0000

    [ADD] 6.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Funcion to find_peak """
+
+
+def find_peak(list_of_integers):
+
+    if list_of_integers is None:
+        return None
+    new_list = list_of_integers.copy()
+    new_list.sort()
+    for i in range(0, len(new_list)):
+        if i == (len(new_list)-1):
+            return(new_list[i])
