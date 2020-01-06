0cb42ce93273cbddda1dc80991eb20d73d8cc71e
Gareth Brickman <977@holbertonschool.com>
2020-01-06 18:17:33 +0000

    [FIX] Make top comment 1 line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-### Displays size of body of response from URL
-### curl -s "$@" | grep -i Content-Length | awk '{print $2}'
+### Displays size of body of response from URL | curl -s "$@" | grep -i Content-Length | awk '{print $2}'
 curl -s "$1" | wc -c
