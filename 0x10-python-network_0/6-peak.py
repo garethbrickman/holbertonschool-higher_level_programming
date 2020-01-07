9e16657b16af615b377720dbf6a6c8e576a0a1c4
Gareth Brickman <977@holbertonschool.com>
2020-01-07 01:39:11 +0000

    [TASK] Try max method for time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,16 @@
 
 def find_peak(list_of_integers):
 
-    if list_of_integers is None:
+    if list_of_integers:
+        new_list = max(list_of_integers)
+        return(new_list)
+    else:
         return None
-    new_list = list_of_integers.copy()
-    new_list.sort()
-    for i in range(0, len(new_list)):
-        if i == (len(new_list)-1):
-            return(new_list[i])
+
+    # if list_of_integers is None:
+    #     return None
+    #     new_list = list_of_integers.copy()
+    #     new_list.sort()
+    #     for i in range(0, len(new_list)):
+    #         if i == (len(new_list)-1):
+    #             return(new_list[i])
