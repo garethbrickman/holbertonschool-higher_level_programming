aa0c2924c5b00cb79df98873768d9efc00b69010
Gareth Brickman <977@holbertonschool.com>
2020-01-13 23:25:10 +0000

    [ADD] 1.

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Fetches website info, displays value of X-Request-Id variable in header
+"""
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
+
+    with request.urlopen(argv[1]) as response:
+        header = dict(response.info())
+        print(header.get("X-Request-Id"))
