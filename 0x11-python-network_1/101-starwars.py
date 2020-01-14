fa9d887fb26bc9be4b395292430587340a799554
Gareth Brickman <977@holbertonschool.com>
2020-01-14 05:19:02 +0000

    [FIX] Pagination

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Takes in a string and sends a search request to the Star Wars API
+    Handles pagination
 """
 if __name__ == "__main__":
     import requests
@@ -11,3 +12,10 @@ if __name__ == "__main__":
     print("Number of results: {}".format(data.get("count")))
     for results in data.get("results"):
         print(results.get("name"))
+
+    if data.get("next") is not None:
+        url = data.get("next")
+        r = requests.get(url)
+        data = r.json()
+        for results in data.get("results"):
+            print(results.get("name"))
