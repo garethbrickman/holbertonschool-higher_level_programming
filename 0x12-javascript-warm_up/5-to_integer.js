d6f2c649640f396f83f2954925d7b373b4e1e73b
Gareth Brickman <977@holbertonschool.com>
2020-02-04 22:19:43 +0000

    [BUGFIX] Correct string

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args2 = parseInt(process.argv[2]);
 if (Number.isInteger(args2) === true) {
-  console.log(`My number is: ${args2}`);
+  console.log(`My number: ${args2}`);
 } else {
   console.log('Not a number');
 }
