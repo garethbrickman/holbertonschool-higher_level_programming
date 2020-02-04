bb9386581593b161caaef68fc25a5b4a5dcc530b
Gareth Brickman <977@holbertonschool.com>
2020-02-04 22:05:41 +0000

    [FEATURE] Incl simpler method for output

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const args2 = process.argv[2];
 const args3 = process.argv[3];
-console.log(args2 + ' ' + 'is' + ' ' + args3);
+console.log(`${args2} is ${args3}`);
+// console.log(args2 + ' ' + 'is' + ' ' + args3);
