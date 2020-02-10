4ee1bbfe28aa72c215a5f7ba4fcdfa8e4acc55f5
Gareth Brickman <977@holbertonschool.com>
2020-02-10 21:43:35 +0000

    [FIX] New line for long if condition, incl comment

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (Number.isInteger(w) !== true || w <= 0 || Number.isInteger(h) !== true || h <= 0) {
+    // won't set attrs if w or h aren't +ve ints > 0
+    if (Number.isInteger(w) !== true || w <= 0 ||
+ Number.isInteger(h) !== true || h <= 0) {
     } else {
       this.width = w;
       this.height = h;
