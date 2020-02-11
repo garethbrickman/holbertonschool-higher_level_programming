7b9cbdbe22e33bac3ee8b1af586efdc762ab0fd3
Gareth Brickman <977@holbertonschool.com>
2020-02-11 04:46:18 +0000

    [TASK] Try true append without text formatting

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
-const endOfLine = require('os').EOL;
+// const endOfLine = require('os').EOL;
 const sourcePath1 = process.argv[2];
 const sourcePath2 = process.argv[3];
 const destPath = process.argv[4];
 
-const content1 = fs.readFileSync(sourcePath1, 'utf8');
-const content2 = fs.readFileSync(sourcePath2, 'utf8');
+const content1 = fs.readFileSync(sourcePath1);
+const content2 = fs.readFileSync(sourcePath2);
 
 const writer = fs.createWriteStream(destPath, {
   flags: 'a'
 });
 writer.write(content1);
-writer.write(endOfLine);
+// writer.write(endOfLine);
 writer.write(content2);
 writer.end();
